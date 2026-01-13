83011ad64ec3724e3a1178cb7bc40bfff36a0714
Jose-Quesada <jose.quesada@fernando3martos.com>
2026-01-13 09:12:16 +0100

    13 enero

---
+++ src/app/gifs/pages/trending-page/trending-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-trending-page',
+  imports: [],
+  templateUrl: './trending-page.html',
+})
+export default class TrendingPage { }
